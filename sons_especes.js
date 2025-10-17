e7860dcdba4ca8ce67149bfef328a68701288472
JulienH77 <84439263+JulienH77@users.noreply.github.com>
2025-10-17 10:09:29 +0200

    Create sons_especes.js

---
+++ sons_especes.js
@@ -0,0 +1,16 @@
+// --- sons mapping ---
+const sonsEspeces = {
+    "Turdus merula": {
+        son: "https://xeno-canto.org/1047850/download",
+        iframe: "https://xeno-canto.org/1047850/embed?simple=1"
+    },
+    "Passer domesticus": {
+        son: "https://xeno-canto.org/1047609/download",
+        iframe: "https://xeno-canto.org/1047609/embed?simple=1"
+    },
+    "Carduelis carduelis": {
+        son: "https://xeno-canto.org/1007384/download",
+        iframe: "https://xeno-canto.org/1007384/embed?simple=1"
+    }
+    // etc...
+};
