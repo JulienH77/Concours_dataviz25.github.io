063735224a7748a100d21a8e17f17ec7ed482632
JulienH77 <84439263+JulienH77@users.noreply.github.com>
2025-10-17 14:40:00 +0200

    Update sons_especes.js

--- sons_especes.js
+++ sons_especes.js
@@ -1,4 +1,44 @@
 window.sonsEspeces = {
+    "Accipiter nisus": {
+        son: "https://xeno-canto.org/1044180/download",
+        iframe: "https://xeno-canto.org/1044180/embed?simple=1"
+    },
+    "Aegithalos caudatus": {
+        son: "https://xeno-canto.org/1046505/download",
+        iframe: "https://xeno-canto.org/1046505/embed?simple=1"
+    },
+    "Apus apus": {
+        son: "https://xeno-canto.org/903877/download",
+        iframe: "https://xeno-canto.org/903877/embed?simple=1"
+    },
+    "Carduelis carduelis": {
+        son: "https://xeno-canto.org/1044169/download",
+        iframe: "https://xeno-canto.org/1044169/embed?simple=1"
+    },
+    "Chloris chloris": {
+        son: "https://xeno-canto.org/1047734/download",
+        iframe: "https://xeno-canto.org/1047734/embed?simple=1"
+    },
+    "Coccothraustes coccothraustes": {
+        son: "https://xeno-canto.org/1000708/download",
+        iframe: "https://xeno-canto.org/1000708/embed?simple=1"
+    },
+    "Columba livia": {
+        son: "https://xeno-canto.org/908646/download",
+        iframe: "https://xeno-canto.org/908646/embed?simple=1"
+    },
+    "Columba palumbus": {
+        son: "https://xeno-canto.org/1038275/download",
+        iframe: "https://xeno-canto.org/1038275/embed?simple=1"
+    },
+    "Corvus corone": {
+        son: "https://xeno-canto.org/1033969/download",
+        iframe: "https://xeno-canto.org/1033969/embed?simple=1"
+    },
+    // etc...
+
+
+    
     "Turdus merula": {
         son: "https://xeno-canto.org/1047850/download",
         iframe: "https://xeno-canto.org/1047850/embed?simple=1"
@@ -6,10 +46,8 @@ window.sonsEspeces = {
     "Passer domesticus": {
         son: "https://xeno-canto.org/1047609/download",
         iframe: "https://xeno-canto.org/1047609/embed?simple=1"
-    },
-    "Carduelis carduelis": {
-        son: "https://xeno-canto.org/1007384/download",
-        iframe: "https://xeno-canto.org/1007384/embed?simple=1"
     }
+
+    
     // etc...
 };
